716daf721023d6faa14ca85671df25c33ce74144
David King <akabogudavid@gmail.com>
2023-03-27 16:08:54 -0700

    printf

--- digit.c
+++ digit.c
@@ -15,9 +15,9 @@ void init_params(params_t *params, va_list ap)
 	params->hashtag_flag =	0;
 	params->zero_flag =	0;
 	params->minus_flag =	0;
-	params->h_modifier = 	0;
-	params->l_modifier = 	0;
+	params->h_modifier =	0;
+	params->l_modifier =	0;
 	params->width =         0;
-        params->precision =     UINT_MAX;
+	params->precision =     UINT_MAX;
 	(void)ap;
 }
